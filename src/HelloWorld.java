d224694c096fd48204fe0c48d6611abb959f184f
Georgios Bizimis <bizigeorge@hotmail.gr>
2022-08-27 09:47:05 -0400

    Added a line to execute Time.Clock();

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,11 @@
 //By convention, class names are capitalized
+
 public class HelloWorld
 {
+
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        Time.Clock();
     }
 }
