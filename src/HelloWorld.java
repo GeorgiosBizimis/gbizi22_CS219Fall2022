93da97232ca67a4896ed77f45080f12e9989c149
Georgios Bizimis <bizigeorge@hotmail.gr>
2022-08-30 15:13:57 -0400

    changed stuff

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,8 +8,18 @@ public class HelloWorld
     {
         System.out.println("Hello World!");
 
+        /*
+            This is a multiline block comment.
+            Another comment line.
+         */
+
         int x = 5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniques of Computer Science";
+        double avogadros = 6.022140857e23;
+        double electron_mass = 9.1093837015e-31;
+
+
     }
 }
