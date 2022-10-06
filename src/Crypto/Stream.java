6baa399c1094001a0080f5892c345604651ece6b
Georgios Bizimis <bizigeorge@hotmail.gr>
2022-10-06 15:10:16 -0400

    StreamCipher

--- src/Crypto/Stream.java
+++ src/Crypto/Stream.java
@@ -1,61 +1,64 @@
 package Crypto;
 
-import java.util.Random;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public final class Stream
 {
+    //all this BS here is because we used to have 2 functions
     private final static int alphabet_length;
     private final static StringBuilder cipher_bldr;
-    private final static Random random;
-    
+    private static SecureRandom random;
+    private final static char base_char;
+
     static {
-        alphabet_length = 26;
+        alphabet_length = (126 - 32) + 1;
         cipher_bldr = new StringBuilder();
-        random = new Random();
+        random = null;
+        base_char = ' ';
+    }
+
+    private static void secureRandomCleanAndDeclare()
+    {
+        try
+        {
+            random = SecureRandom.getInstance("SHA1PRNG");
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void clearStringBuilder() { cipher_bldr.setLength(0); }
 
-    public static String E(final String clear, final int key)
+    public static String E(final String clear, final int key, final boolean encrypt)
     {
         clearStringBuilder();
-
+        secureRandomCleanAndDeclare();
         random.setSeed(key);
 
         for(int i = 0; i < clear.length(); i++)
         {
-            final int shift = random.nextInt(100);
+            final int shift;
+            if(encrypt) shift = random.nextInt(100);
+            else shift = -random.nextInt(100);
+
             System.out.println(shift);
-            final int cipher_char_pos = Math.floorMod(clear.charAt(i) - 'a' + shift, alphabet_length);
-            final char cipher_char = (char) (cipher_char_pos + 'a');
+            final int cipher_char_pos = Math.floorMod(clear.charAt(i) - base_char + shift, alphabet_length);
+            final char cipher_char = (char) (cipher_char_pos + base_char);
             cipher_bldr.append(cipher_char);
         }
         
         return cipher_bldr.toString();
     }
 
-    public static String D(final String cipher, final int key)
-    {
-        clearStringBuilder();
 
-        random.setSeed(key);
-
-        for(int i = 0; i < cipher.length(); i++)
-        {
-            final int shift = random.nextInt(100);
-            System.out.println(shift);
-            final int cipher_char_pos = Math.floorMod(cipher.charAt(i) - 'a' - shift, alphabet_length);
-            final char cipher_char = (char) (cipher_char_pos + 'a');
-            cipher_bldr.append(cipher_char);
-        }
-
-        return cipher_bldr.toString();
-    }
 
     public static void main(String[] args)
     {
-        System.out.println(E("java", 20)); //encryption test
-        System.out.println(D("kkwj", 20)); //decryption test
+        System.out.println(E("java", 20, true)); //encryption test
+        System.out.println(E("/S&Y", 20, false));
     }
     
 }
