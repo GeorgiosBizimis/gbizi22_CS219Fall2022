464f60c92d571b0107d1d33e6b9662aa1c277b5e
Georgios Bizimis <bizigeorge@hotmail.gr>
2022-10-09 01:45:53 -0400

    SwallowStats

---
+++ src/HW6/SwallowStats.java
@@ -0,0 +1,82 @@
+/*
+ *  Georgios Bizimis
+ *  for 10 / 12 / 2022
+ *  HW6 - SwallowStats
+ */
+
+package HW6;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public final class SwallowStats
+{
+
+    private static Scanner URLScanner(final String url_string)
+    {
+        final URL url; // null is the nothing value
+
+        try {
+            url  = new URL(url_string);
+            return new Scanner(url.openConnection().getInputStream());
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void swallow_stats()
+    {
+        // connect to the webpage of speeds
+        final Scanner s = URLScanner("http://10.60.15.25/~ehar/cs219/swallow-speeds.txt");
+
+        double sum = 0;
+        int count = 0;
+        double min = 0;
+        double max = 0;
+
+        boolean initialized = false;
+
+        while(true)
+        {
+            try {
+                final String line = s.nextLine();
+                if(!line.contains("#") && !line.isBlank())
+                {
+                    final double number = Double.parseDouble(line);
+                    System.out.println(number);
+                    sum += number;
+                    count++;
+
+                    if(initialized)
+                    {
+                        if(number < min) min = number;
+                        if(number > max) max = number;
+                    }
+                    else
+                    {
+                        min = Double.parseDouble(line);
+                        max = Double.parseDouble(line);
+                        initialized = true;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                break;
+            }
+        }
+        System.out.println();
+        System.out.printf("The average swallow speed is %.2f\n", sum/count);
+        System.out.printf("The minimum swallow speed is %.2f\n", min);
+        System.out.printf("The maximum swallow speed is %.2f\n", max);
+    }
+
+    public static void main(String[] args)
+    {
+        swallow_stats();
+    }
+
+}
